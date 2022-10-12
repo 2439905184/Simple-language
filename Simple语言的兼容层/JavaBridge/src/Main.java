f5a284227d8048c95cb6916b79f8dea2ab8aaa48
2439905184 <suchenglong018@126.com>
2022-10-12 19:54:10 +0800

    ✨ feat: Simple语言的Java反射兼容层
    Simple语言的Java反射兼容层

---
+++ Simple语言的兼容层/JavaBridge/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import Simple;
+
+public class Main
+{
+	//public static String a = "";
+    public String dymatic_old = "动态变量:初始值";
+    public static String static_old = "静态变量:初始值";
+    public static Main self;
+    //在运行时动态修改变量的值 动态反射
+    private static void dymatic_set() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException
+    {
+        System.out.println("动态反射前"+self.dymatic_old);
+        //动态反射
+        Simple.exe(self,"动态反射1");
+        Simple.exe(self,"动态反射2");
+        Simple.exe(self,"动态反射3");
+        Simple.exe(self,"动态反射4");
+        System.out.println("动态反射最终值 "+self.dymatic_old);
+    }
+    //在运行时动态修改变量的值 静态反射
+    private static void static_set() throws IllegalArgumentException, NoSuchFieldException, IllegalAccessException
+    {
+        System.out.println("静态反射前 "+static_old);
+        Simple.exe(Main.class,"静态反射1->static_old");
+        Simple.exe(Main.class,"静态反射2->static_old");
+     /*   Simple.exe(Main.class,"静态反射3");
+        Simple.exe(Main.class,"静态反射4");*/
+        System.out.println("静态反射最终值 "+static_old);
+    }
+	public static void main(String[] args) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+    { 
+        //引用
+        self=new Main(); 
+        //dymatic_set();
+        static_set();
+    }
+    
+}
---
+++ Simple语言的兼容层/JavaBridge/src/Simple.java
@@ -0,0 +1,32 @@
+import java.lang.reflect.Field;
+public class Simple
+{
+    // 静态反射
+    public static void exe(Object ClsObj,String value) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+    {
+        Field field = ClsObj.getClass().getField("dymatic_old");
+        field.set(ClsObj,value);
+        System.out.println(field.get(ClsObj));
+    }
+    // 参数2是simple语言的代码
+    public static void exe(Class<?> cls,String simple_code) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+    {      
+        String[] sp = simple_code.split("->");
+        //java变量值
+        String value = sp[0];
+        //java变量名
+        String var = sp[1];
+        //变量名
+        Field field =  cls.getField(var);
+        field.set(cls,value);
+        System.out.println("反射后 "+field.get(cls));
+        // todo 制作函数调用和字面量的判断,词法分析,函数调用的词法分析暂时只支持 void fx() 这种形式
+    }
+    //在运行时反射 创建新的变量并绑定到反射的类 可能无法实现
+    public static void test(Object ClsObj)
+    {
+        String a  = "动态绑定";
+        Class<?> cls = ClsObj.getClass();
+     //   cls.getField("j").set
+    }
+}
