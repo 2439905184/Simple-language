778fb29dc4313aace1c7987f38f393d65d827d92
2439905184 <suchenglong018@126.com>
2022-10-14 17:45:11 +0800

    🌈 style: 同步

--- Simple语言的兼容层/C/MyTest.c
+++ Simple语言的兼容层/C/MyTest.c
@@ -5,10 +5,5 @@ int main()
     // 声明两个变量
     int v = 0;
     change_int(&v,128);
-    
-    //int vv = 1;
-    // 传入变量的内存地址
-    //change_value(&v,&vv);
     printf("%d",v);
-    // printf("%d",v);
 }
--- Simple语言的兼容层/C/Simple.h
+++ Simple语言的兼容层/C/Simple.h
@@ -21,3 +21,8 @@ void change_variant()
 {
 
 }
+// todo
+void execute()
+{
+
+}
