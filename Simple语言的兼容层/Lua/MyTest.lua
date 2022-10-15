96158a0b783a47481be67251d36854e546b714ab
2439905184 <suchenglong018@126.com>
2022-10-15 20:19:26 +0800

    🐞 fix: 修复lua为引用传递

--- Simple语言的兼容层/Lua/MyTest.lua
+++ Simple语言的兼容层/Lua/MyTest.lua
@@ -1,8 +1,9 @@
-old = "old"
-
-function change_value() 
-    old = "new"
+local test = { old="old" }
+-- 在lua中除了 table是引用传递 外，其余基本都是值传递。所以当你直接打印一个table的时候，看到的是一个指针类型的数据
+function change_value(var1 ,var2) 
+    var1.old = var2
 end
 
+change_value(test,"new")
 
-print(old)
+print(test.old)
