8eb5cfee80f8c85eea3697372c925b858680cc13
2439905184 <suchenglong018@126.com>
2022-10-15 17:39:45 +0800

    🐞 fix: 修复无法修改值

--- Simple语言的兼容层/Js/MyTest.mjs
+++ Simple语言的兼容层/Js/MyTest.mjs
@@ -1,4 +1,12 @@
 //es6
-import a from "simple"
+var test = {"old":"old"}
+// var old = "old"
+// js的参数传递, object是传递的实际内存地址 普通变量参数传入函数时, 会创建一个拷贝引用 所以无法修改原变量的值
+function change_value(var_name, var_value)
+{
+    var_name.old = var_value
+    //console.log(var_name)
+}
 
-console.log(a)
+change_value(test,"new")
+console.log(test)
